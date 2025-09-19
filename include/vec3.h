fffbe85418438ae88461ab6d480b98b2e8e89be4
Ethanboyz <ethanboyz10@gmail.com>
2025-09-18 23:30:52 -0400

    Basic vector-vector arithmetic, unit vector functions

--- include/vec3.h
+++ include/vec3.h
@@ -50,6 +50,11 @@ public:
         return *this;
     }
 
+    friend std::ostream& operator<<(std::ostream& stream, const vec3& v) noexcept {
+        stream << "[" << v.s[0] << ", " << v.s[1] << ", " << v.s[2] << "]";
+        return stream;
+    }
+
     // Scalar-vector arithmetic
     friend constexpr vec3 operator+(const vec3& u, const vec3& v) noexcept {
         return vec3{u.s[0] + v.s[0], u.s[1] + v.s[1], u.s[2] + v.s[2]};
@@ -67,11 +72,29 @@ public:
         return vec3{v.s[0] / t, v.s[1] / t, v.s[2] / t};
     }
 
+    // Vector-vector arithmetic
+    friend float dot(const vec3& u, const vec3& v) {
+        return u.s[0] * v.s[0] + u.s[1] * v.s[1] + u.s[2] * v.s[2];
+    }
+
+    friend vec3 cross(const vec3& u, const vec3& v) {
+        return vec3{
+            u.s[1] * v.s[2] - u.s[2] * v.s[1],
+            u.s[2] * v.s[0] - u.s[0] * v.s[2],
+            u.s[0] * v.s[1] - u.s[1] * v.s[0],
+        };
+    }
+
+    // Misc
+
     float length_squared() const noexcept {
         return s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
     }
     float length() const noexcept {
         return std::sqrt(this->length_squared());
+    }
+    friend vec3 unit_vec3(const vec3& v) {
+        return v / v.length();
     }
 };
 
