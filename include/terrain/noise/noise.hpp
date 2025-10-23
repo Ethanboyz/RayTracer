34ac500651c90410c5ce38784cd9a8a033b2abe4
Ethanboyz <ethanboyz10@gmail.com>
2025-10-23 16:43:46 -0400

    Opensimplex noise function implementation (credits to K.jpg's Opensimplex2S impl)

--- include/terrain/noise/noise.hpp
+++ include/terrain/noise/noise.hpp
@@ -6,13 +6,14 @@
  * @brief Describes a noise function that provides a height value for each given Heightmap (x, z) coordinate.
  */
 class Noise {
+public:
     virtual ~Noise() = default;
 
     /**
-     * @return Height value (0.0-1.0) of a specified x and z-coordinate pair in heightmap-space.
+     * @return Height value (-1.0 - 1.0) of a specified x and z-coordinate pair in heightmap-space.
      * @note Return result should be used to construct a Heightmap.
      */
-    virtual float noise(int x, int z) const = 0;
+    virtual float eval(int x, int z) const = 0;
 };
 
 #endif
